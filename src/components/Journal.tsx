60185bb9f7f4d09462a5582cca9104117eddfe8d
kestermbah <156464417+kestermbah@users.noreply.github.com>
2024-12-25 14:03:22 -0500

    Fixed the text area

--- src/components/Journal.tsx
+++ src/components/Journal.tsx
@@ -1,11 +1,21 @@
 import "./Journal.css";
-
+import TextareaAutosize from "react-textarea-autosize";
+import { useState } from "react";
 const Journal = () => {
+  const [entry, setEntry] = useState("");
   return (
     <div className="Journal">
       <h1 className="Title">Journal Entry</h1>
       <div className="Entry">
-        <p className="Content">Write...</p>
+        <TextareaAutosize
+          onChange={(e) => setEntry(e.target.value)}
+          className="Content"
+          placeholder="Write..."
+        />
+      </div>
+      <div className="Actions">
+        <button className="Button">Talk to AI</button>
+        <button className="Button">Sumbit</button>
       </div>
     </div>
   );
